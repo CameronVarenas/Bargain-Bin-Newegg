90c3abc9bddb012508856e9af0b06246d1fb0c29
Cameron Varenas <camerontvarenas@gmail.com>
2021-05-07 11:01:47 -0400

    Built functionality for object methods: getSession and logout. In authController.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,10 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     getSession: (req, res) => {
-
+        if (req.session.user) {
+            return res.status(200).send(req.session.user)
+        };
+        res.sendStatus(403);
     },
 
     register: (req, res) => {
@@ -14,6 +17,7 @@ module.exports = {
     },
 
     logout: (req, res) => {
-
+        req.session.destroy();
+        return res.sendStatus(200);
     }
 };
