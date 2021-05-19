bd25db0f15d6c88878ae3a8895e693a47da11e68
Cameron Varenas <camerontvarenas@gmail.com>
2021-05-19 13:19:25 -0400

    Added an extra join statement to get_recent_reviews.sql to include product images into RecentReviews.js

--- db/user_reviews/get_recent_reviews.sql
+++ db/user_reviews/get_recent_reviews.sql
@@ -1,4 +1,5 @@
 SELECT * FROM bbn_product_reviews
 JOIN bbn_products ON bbn_product_reviews.product_id = bbn_products.product_id
+JOIN bbn_product_img ON bbn_product_reviews.product_id = bbn_product_img.product_id
 ORDER BY timestamp DESC
 LIMIT 10;
